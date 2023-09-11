b7b99f084dddbe523c9589cff86733c053a0db0f
po-gl <porterglines@gmail.com>
2023-09-10 19:53:33 -0600

    Enable proxy target for api requests

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import macrosPlugin from 'vite-plugin-babel-macros'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), macrosPlugin()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react(), macrosPlugin()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_PROXY_TARGET || 'http://localhost:3000',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      }
+    }
+  };
 })
