baba682f07f2a90ad78a46d54b6ca1e1727c7ebd
po-gl <porterglines@gmail.com>
2023-09-16 14:15:34 -0600

    Return list of endpoints at root "/"

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -6,6 +6,19 @@ import bodyParser from 'body-parser';
 const routes: Router = Router();
 routes.use(bodyParser.json());
 
+routes.get('/', async (req, res) => {
+  const route_endpoints = routes.stack
+    .map((layer) => layer.route)
+    .filter((route) => route !== undefined)
+    .map((route) => {
+      return {
+        endpoint: route.path,
+        methods: route.methods
+      }
+    })
+  res.json({ endpoints: route_endpoints });
+});
+
 routes.put('/map', async (req, res) => {
   res.send('Hia there');
 });
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,27 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { connectToDB, getDB } from './db.js';
-import getOpenAI from './ml.js';
+import { connectToDB } from './db.js';
 import routes from './routes.js';
 
 dotenv.config();
 
 const app = express();
-
-app.get('/', async (req, res) => {
-  const db = getDB();
-  const maps = await db.collection('maps').find({}).toArray();
-  res.send(maps);
-});
-
-app.get('/openai', async (req, res) => {
-  const openai = getOpenAI();
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: 'user', content: 'Say this is a test and then name a random animal' }],
-    model: 'gpt-3.5-turbo',
-  });
-  res.send(completion.choices);
-});
 
 app.use(routes);
 
