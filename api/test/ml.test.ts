40931954c1db4c5b18ef96a5ec07f3be4870f0a5
po-gl <porterglines@gmail.com>
2023-09-17 23:40:01 -0600

    Move setup inside describe block and change test name wording

--- api/test/ml.test.ts
+++ api/test/ml.test.ts
@@ -1,28 +1,28 @@
 import { jest, describe, expect, test, beforeEach } from '@jest/globals';
 import ml from '../src/ml.js';
 
-beforeEach(() => {
-  const spy = jest.spyOn(ml, 'getOpenAI');
-  // @ts-ignore
-  spy.mockImplementation(() => {
-    const payload = `{
+describe('ml module', () => {
+
+  beforeEach(() => {
+    const spy = jest.spyOn(ml, 'getOpenAI');
+    // @ts-ignore
+    spy.mockImplementation(() => {
+      const payload = `{
       "nodes": [ { "id": 1, "text": "test" }, { "id": 2, "text": "jest" }, { "id": 3, "text": "best" } ],
       "edges": [ { "source_id": 1, "target_id": 2, "justification": "testing" }, { "source_id": 1, "target_id": 3, "justification": "" } ]
     }`
-    return { chat: { completions: { create: async () => { return { choices: [{ message: { function_call: { arguments: payload } } }], } }, }, }, };
+      return { chat: { completions: { create: async () => { return { choices: [{ message: { function_call: { arguments: payload } } }], } }, }, }, };
+    });
   });
-});
-
-describe('ml module', () => {
 
   describe('generating mind maps', () => {
 
-    test('a valid generation does not throw', async () => {
+    test('should not throw for a valid generation', async () => {
       const map = await ml.generateMindmap(['test', 'jest']);
       expect(typeof map).toBe('string');
     });
 
-    test('an invalid generation throws', async () => {
+    test('should throw for an invalid generation', async () => {
       const spy = jest.spyOn(ml, 'getOpenAI');
       // @ts-ignore
       spy.mockImplementation(() => {
