ce827fae47a26fc4b7cb9ef79e515c70f3cae9e8
po-gl <porterglines@gmail.com>
2023-09-26 17:50:45 -0600

    Add ErrorPage

---
+++ ui/src/components/ErrorPage.tsx
@@ -0,0 +1,21 @@
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+import './styles/ErrorPage.css';
+
+function ErrorPage() {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div className="error-page">
+      <h1>Error!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      {isRouteErrorResponse(error) &&
+        <p>
+          <i>{error.status}: {error.statusText || error.data.message}</i>
+        </p>
+      }
+    </div>
+  );
+}
+
+export default ErrorPage;
--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import ErrorPage from './components/ErrorPage.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
